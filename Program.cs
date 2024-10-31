335e758da9d392ebb507908bb8e560f1a453b560
Luis Coco Enríquez <LEnriquez@luxoft.com>
2024-10-31 13:07:50 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Extensions.AI;
+
+var endpoint = new Uri("https://cocoe-m2ae1t7j-swedencentral.openai.azure.com/");
+var credentials = new AzureKeyCredential("");
+var deploymentName = "gpt-4o";
+
+IChatClient client = new AzureOpenAIClient(endpoint, credentials).AsChatClient(deploymentName);
+
+var response = await client.CompleteAsync("What is AI?");
+
+Console.WriteLine(response.Message);
+
